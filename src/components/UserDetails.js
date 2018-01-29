0cbd6008f978fd8c8ad0b4a1f79b9bcae6f75036
darkroom51 <darkroom@wp.pl>
2018-01-29 15:28:05 +0100

    fetch random user

--- src/components/UserDetails.js
+++ src/components/UserDetails.js
@@ -1,11 +1,23 @@
 import React from 'react'
+import {withRouter} from 'react-router-dom'
 
 const styles={
 
 }
 
-const UserDetails = props => (
-    <div></div>
-)
+const UserDetails = props => {
+    const arrWithUsers = props.usersData || []
+    const currentUserEmail = props.match.params.email
+    const currentUserData = arrWithUsers.filter((el) => el.email === currentUserEmail)
 
-export default UserDetails
+    return (
+        <div>
+            {props.match.params.email}
+            {
+                JSON.stringify(currentUserData)
+            }
+        </div>
+    )
+}
+
+export default withRouter(UserDetails) //komponent wyzszego rzedu withRouter, dzieki temu mozna bylo przekazac params przy zastowowaniu render
